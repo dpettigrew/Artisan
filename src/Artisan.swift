08f565f3e04a0bf56fa6c8b6542c25d43bd3af28
David Pettigrew <dpettigrew@walmartlabs.com>
2016-07-14 22:17:28 -0700

    swift 3 API changes

--- src/Artisan.swift
+++ src/Artisan.swift
@@ -1,9 +1,8 @@
 //
 //  Artisan.swift
-//  ArtisanSample
 //
 //  Created by David Pettigrew on 4/7/15.
-//  Copyright (c) 2015 Walmart Labs. All rights reserved.
+//  Copyright (c) 2015 LifeCentrics. All rights reserved.
 //
 
 import UIKit
